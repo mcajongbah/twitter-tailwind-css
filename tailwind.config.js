1cf3a0f4f5bb141f805e285504bd9744c15c8381
mcajongbah <malvinajongbah@gmail.com>
2021-01-13 14:00:21 +0000

    side nav done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,44 @@
 module.exports = {
   purge: [],
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#1da1f2",
+        secondary: "#F3F4F6",
+      },
+      width: {
+        "1/10": "275px",
+        "2/10": "600px",
+        "3/10": "405px",
+      },
+    },
+    container: {
+      center: true,
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        ".container": {
+          maxWidth: "100%",
+          "@screen sm": {
+            maxWidth: "640px",
+          },
+          "@screen md": {
+            maxWidth: "768px",
+          },
+          "@screen lg": {
+            maxWidth: "1024px",
+          },
+          "@screen xl": {
+            maxWidth: "1280px",
+          },
+        },
+      });
+    },
+  ],
+};
