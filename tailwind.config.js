9f6408972786845d7cae08132c030cdd26f7ecc4
mcajongbah <malvinajongbah@gmail.com>
2021-04-03 18:49:04 +0000

    attempt at responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,9 @@ module.exports = {
         "3/10": "405px",
       },
       gridTemplateColumns: {
+        13: "repeat(13, minmax(0, 1fr))",
+        14: "repeat(14, minmax(0, 1fr))",
+        15: "repeat(15, minmax(0, 1fr))",
         16: "repeat(16, minmax(0, 1fr))",
       },
     },
